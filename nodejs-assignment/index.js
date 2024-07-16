b994b80354555acefc19cf79c60f19f442f1af02
Divy001 <divyashakti131@gmail.com>
2024-07-16 22:29:52 +0530

    added mongodb connection message

--- nodejs-assignment/index.js
+++ nodejs-assignment/index.js
@@ -87,5 +87,6 @@ app.get("/", async (req, res) => {
 });
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log("MongoDb Connected")
 });
